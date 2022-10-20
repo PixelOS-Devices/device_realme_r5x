f37f06c45e6bd77db2895363f299cc9e46c7e3a2
Sugakesshaa <83747297+bheatleyyy@users.noreply.github.com>
2022-10-20 08:41:31 +0800

    ok

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
