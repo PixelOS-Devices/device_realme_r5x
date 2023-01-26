95182cd02f1fb435c5dee245e57932da993d22fc
Om Patel <89215836+takumi021@users.noreply.github.com>
2023-01-26 11:16:26 +0530

    Ok

--- device.mk
+++ device.mk
@@ -533,4 +533,4 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/realme/r5x/r5x-vendor.mk)
 
 # RealmeDirac
-$(call inherit-product, $(LOCAL_PATH)/RealmeDirac/dirac.mk)
+#$(call inherit-product, $(LOCAL_PATH)/RealmeDirac/dirac.mk)
